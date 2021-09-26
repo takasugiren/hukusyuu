5c89f6801e21902a38fe594d74d15ba18b547fa9
takasugiren <bucyou16528080@gmail.com>
2021-09-26 13:43:26 +0000

    バリデーション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,13 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.save
-    flash[:notice] = 'Book was successfully created.'
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -21,9 +25,12 @@ class BooksController < ApplicationController
 
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    flash[:notice] = 'Book was successfully updated.'
-    redirect_to book_path(@book)
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book)
+    else
+      render :edit
+    end
   end
 
   def destroy
